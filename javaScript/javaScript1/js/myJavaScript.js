e6a7cd14ecc88319b121bc26bda5beff38cd4121
MichaelJames0913 <mjschmid@ucsc.edu>
2017-10-05 20:22:02 -0700

    javaScript

---
+++ javaScript/javaScript1/js/myJavaScript.js
@@ -0,0 +1,6 @@
+	// this is a javaScript file 
+	// I can use // to make comments
+
+	function hello() { 
+        alert ( "Wecome to javaScript!!!") // basic alert 
+    }
---
+++ javaScript/javaScript1/readme.txt
@@ -0,0 +1,2 @@
+this is not my first javaScript Program, but what is important is how the javaScrip file is in separate file
+and that it is reference from index.html 
